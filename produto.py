264a755197b6dc96f4ac7dfd93bc3d7f68665562
Nicolas Henrique Costa Santos <nh157040@gmail.com>
2026-03-03 15:14:06 -0300

    Update produto.py

--- produto.py
+++ produto.py
@@ -3,10 +3,11 @@ class Produto:
     def __init__(self, nome, preco, quantidade):
         # Atributos principais do produto
         self.nome = nome              # Nome do produto
-        self.preco = preco            # Preço unitário
+        self.preco = preco            # Preço unitário( Multiplicação)
         self.quantidade = quantidade  # Quantidade adicionada ao carrinho
 
     # Método que calcula o valor total daquele produto
     # (preço unitário multiplicado pela quantidade)
     def subtotal(self):
+
         return self.preco * self.quantidade
